4b38c4e647b2eab784f509a0566ceaf3ff286406
amr <freshwater@users.noreply.github.com>
2020-11-03 18:19:46 -0800

    \

--- eto.py
+++ eto.py
@@ -16,7 +16,6 @@ character_data = np.array(data['Character'], dtype=np.uint8)
 state = {
     'ProgramCounter': 0,
     'Accumulator': 0, 'X': 0, 'Y': 0,
-    'StackRegister': 0,
     'StatusRegister': {
         'Negative': 0,
         'Zero': 0,
@@ -39,67 +38,134 @@ def reset():
     state['StackZero'] = 0x0100
     state['StackOffset'] = 0xFD
 
-def increment():
-    opcode = state['Memory'][state['ProgramCounter']]
-    state['ProgramCounter'] += 1
-
-    return opcode
-
-def vertical_blank(): state['Memory'][0x2002] = 0x80
-
 ops = {}
 
 def visit(index, text):
     count, text = ops.get(index, (0, text))
     ops[index] = (count + 1, text)
 
-vertical_blank()
-reset()
+def increment():
+    opcode = state['Memory'][state['ProgramCounter']]
+    state['ProgramCounter'] += 1
+
+    return opcode
 
 indexes = []
 
+# 341 PPU cycles per scanline
+# 113.667 CPU cycles per scanline
+# 261 scanlines
+# 241-261 is vblank
+
+PPU_CONTROL = 0x2000
+PPU_STATUS = 0x2002
+
 def do(n):
-    global last
+    for _1 in range(999999999):
+        for v in range(-1, 261):
+            vblank = None
 
-    for _1 in range(n):
-        index = state['ProgramCounter']
-        opcode = increment()
-        indexes.append(index)
+            for h in range(341):
 
-        operation, byte_count, addressing = instructions[opcode.item()]
+                if vblank == None and v == 241:
+                    if h == 2:
+                        state['Memory'][PPU_STATUS] = 0x80
 
+                        if state['Memory'][PPU_CONTROL] & 0x80:
+                            # return n, v, h
+                            NMI(state)
+
+                        vblank = True
+
+                n -= 1
+                if n == 0:
+                    return _1, v, h
+
+                if n % 3 == 0:
+                    index = state['ProgramCounter']
+                    opcode = increment()
+                    indexes.append(index)
+
+                    operation, byte_count, addressing = instructions[opcode.item()]
+                    visit_data = {'opcode': opcode, 'byte_count': byte_count,
+                                  'operation': operation.__name__, 'addressing': addressing}
+
+                    if byte_count == 1:
+                        # here. merge dicts
+                        visit(index, visit_data)
+                        operation(state, 0)
+
+                    elif byte_count == 2:
+                        data = increment()
+
+                        fy = ",y" if addressing == indy else ""
+
+                        visit(index, visit_data | {'data': data})
+                        operation(state, addressing(state, data))
+
+                    elif byte_count == 3:
+                        data1, data2 = increment(), increment()
+
+                        fy = ",y" if addressing == absy else ""
+                        visit(index, visit_data | {'data': data2*0x0100 + data1})
+                        operation(state, addressing(state, data1, data2))
+
+                    else:
+                        1 / 0
+
+# do(12708 + 6611 + 1000)
+
+# vertical_blank()
+reset()
+
+labels = {
+    "0778": "MIRROR_PPU_CTRL_1",
+    "2000": "PPU_CTRL_1",
+    "2001": "PPU_CTRL_2",
+    "2002": "PPU_STATUS",
+    "2006": "PPU",
+    "2007": "PPU_IO",
+    "4014": "SPRITE",
+    "8082": "NMI",
+    "809E": "ScreenOff",
+    "8E92": "WriteBuffer",
+    "8EDD": "UpdateScreen",
+    "8EED": "WritePPUReg1",
+    "90CC": "InitializeMemory",
+}
+
+labels = {int(key, 16): value for key, value in labels.items()}
+l = lambda i: f'{labels.get(i, ""):15}'
+
+import sys
+_1, v, h = do(int(sys.argv[1]))
+
+def r(byte_count, data, addressing):
+    if data != None:
         if byte_count == 1:
-            visit(index, f'{opcode:02X} {operation.__name__}')
-            operation(state, 0)
-
+            return ''
         elif byte_count == 2:
-            data = increment()
-
-            fy = ",y" if addressing == indy else ""
-        
-            visit(index, f'{opcode:02X} {operation.__name__} .${data:02x}' + fy)
-            operation(state, addressing(state, data))
-
+            if addressing == indy:
+                return f'${data:02X},y'
+            else:
+                return f'${data:02X}'
         elif byte_count == 3:
-            data1, data2 = increment(), increment()
-
-            fy = ",y" if addressing == absy else ""
-            visit(index, f'{opcode:02X} {operation.__name__} ${data2:02x}{data1:02x}{fy} ;{data1*0x0100 + data2:04x}')
-            operation(state, addressing(state, data1, data2))
-
-        else:
-            1 / 0
-
-do(12708 + 6611 + 1000)
-opcode = state['Memory'][state['ProgramCounter']]
+            return  f'${data:04X} {labels.get(data, "")}'
+    else:
+        return ''
 
 b = {'False': " ", '0': '●', '1': '•', '2': '⋅', '3': '⋅'}
 a = lambda i: b[str((i in indexes[-4:]) and indexes[-4:][::-1].index(i))]
 ops = sorted(ops.items())
-print('\n' + '\n'.join([f'{a(index)}{count:2} {index:02X} {op}' for index, (count, op) in ops]) + '\n')
+print('\n' + '\n'.join([
+    f'{l(index)}{a(index)}{count:4} {index:02X} .{d["opcode"]:02X} {d["operation"]} {r(d["byte_count"], d.get("data"), d["addressing"])}'
+    for index, (count, d) in ops]) + '\n')
 
 print()
+opcode = state['Memory'][state['ProgramCounter']]
 print(hex(state['ProgramCounter']), state, hex(opcode.item()), opcode.item())
+print()
+print("i", _1, "v", v, "h", h)
 print()
 
 # print(f":{state['Memory'][0x04A0]:X}")
--- operations.py
+++ operations.py
@@ -17,6 +17,9 @@ def imm(state, data):
 def imm2(state, data1, data2):
     return data2*0x0100 + data1
 
+def zpg(state, data):
+    return state['Memory'][data]
+
 def indx(state, data):
     address = data + state['X']
     return state['Memory'][(address + 1)*0x0100 + address*data]
@@ -30,13 +33,17 @@ def absy(state, data1, data2):
     return state['Memory'][address + state['Y']]
 
 def abs_read(state, data1, data2):
-    print(f'{data2*256 + data1:04x} -> {state["Memory"][data2*0x0100 + data1]}')
+    address = data2*0x0100 + data1
+    # print(f'{address:04x} -> {state["Memory"][address]}')
+    result = state['Memory'][address]
+    if address == 0x2002:
+        state['Memory'][0x2002] &= 0x7F
 
-    return state['Memory'][data2*0x0100 + data1]
+    return result
 
 def abs_x_read(state, data1, data2):
     x = state['X']
-    print(f'{data2*256 + data1:04x} + {x:02x} -> {state["Memory"][data2*0x0100 + data1 + x]}')
+    # print(f'{data2*256 + data1:04x} + {x:02x} -> {state["Memory"][data2*0x0100 + data1 + x]}')
 
     return state['Memory'][data2*0x0100 + data1 + x]
 
@@ -46,6 +53,7 @@ def abs_x_read(state, data1, data2):
 
 def SEI(state, a) -> [(0x78, _)]: state['StatusRegister']['Interrupt'] = 1
 def CLD(state, a) -> [(0xD8, _)]: state['StatusRegister']['Decimal'] = 0
+def SEC(state, a) -> [(0x38, _)]: state['StatusRegister']['Carry'] = 1
 
 def Z_set(state, value):
     state['StatusRegister']['Zero'] = 1*(value == 0)
@@ -53,10 +61,10 @@ def Z_set(state, value):
 def N_set(state, value):
     state['StatusRegister']['Negative'] = 1*(value & 0x80 == 0x80)
 
-def LDA(state, a) -> [(0xA9, imm), (0xAD, abs_read), (0xBD, abs_x_read)]:
+def LDA(state, a) -> [(0xA9, imm), (0xAD, abs_read), (0xBD, abs_x_read), (0xB1, indy)]:
     state['Accumulator'] = a; Z_set(state, a); N_set(state, a)
-def LDX(state, a) -> [(0xA2, imm)]: state['X'] = a; Z_set(state, a); N_set(state, a)
-def LDY(state, a) -> [(0xA0, imm)]: state['Y'] = a; Z_set(state, a); N_set(state, a)
+def LDX(state, a) -> [(0xA2, imm), (0xAE, abs_read)]: state['X'] = a; Z_set(state, a); N_set(state, a)
+def LDY(state, a) -> [(0xA0, imm), (0xAC, abs_read)]: state['Y'] = a; Z_set(state, a); N_set(state, a)
 def DEX(state, a) -> [(0xCA, _)]: state['X'] = byte(state['X'] - 1); Z_set(state, state['X']); N_set(state, state['X'])
 def DEY(state, a) -> [(0x88, _)]: state['Y'] = byte(state['Y'] - 1); Z_set(state, state['Y']); N_set(state, state['Y'])
 def INY(state, a) -> [(0xC8, _)]: state['Y'] = byte(state['Y'] + 1); Z_set(state, state['Y']); N_set(state, state['Y'])
@@ -69,7 +77,7 @@ def BIT(state, a) -> [(0x2C, abs_read)]:
     state['StatusRegister']['Overflow'] = (a >> 6) & 0x01
 
 def CMP(state, a) -> [(0xC9, imm)]:
-    result = state['Accumulator'] - a
+    result = byte(state['Accumulator'] - a)
     Z_set(state, result); N_set(state, result)
     if a <= state['Accumulator']:
         state['StatusRegister']['Carry'] = 1
@@ -95,16 +103,70 @@ def CPY(state, a) -> [(0xC0, imm)]:
 def ORA(state, a) -> [(0x09, imm)]: state['Accumulator'] |= a; Z_set(state, state['Accumulator']); N_set(state, state['Accumulator'])
 def EOR(state, a): state['Accumulator'] ^= a; Z_set(state, state['Accumulator']); N_set(state, state['Accumulator'])
 def AND(state, a) -> [(0x29, imm)]: state['Accumulator'] &= a; Z_set(state, state['Accumulator']); N_set(state, state['Accumulator'])
+def ASL(state, a) -> [(0x0A, _)]:
+    result = state['Accumulator'] << 1
+
+    Z_set(state, byte(result))
+    N_set(state, byte(result))
+    state['StatusRegister']['Carry'] = 1*(result & 0xFF00 > 0)
+
+    state['Accumulator'] = byte(result)
+
+def LSR(state, a) -> [(0x4A, _)]:
+    result = state['Accumulator'] >> 1
+
+    Z_set(state, result)
+    N_set(state, result)
+    state['StatusRegister']['Carry'] = state['Accumulator'] & 0x01
+
+    state['Accumulator'] = result
+
+once = set()
+def ADC(state, a) -> [(0x65, zpg)]:
+    """
+    A + M + C -> A, C                N Z C i d V
+
+    The overflow flag is set
+    when the sign or bit 7 is changed due to the result exceeding +127
+    or -128, otherwise overflow is reset.
+    """
+
+    result = state['Accumulator'] + a + state['StatusRegister']['Carry']
+
+    Z_set(state, byte(result))
+    N_set(state, byte(result))
+    state['StatusRegister']['Carry'] = 1*(result & 0xFF00 > 0)
+
+    v1 =  ~(state['Accumulator'] ^ a) & (state['Accumulator'] ^ result) & 0x0080
+    flag_0 = state['Accumulator'] >> 7
+    flag_1 = result >> 7
+    v2 = (flag_1 != flag_0) and state['StatusRegister']['Carry']
+
+    state['StatusRegister']['Overflow'] = v1
+    if v1 != 1*v2:
+        if (v1, v2) not in once:
+            once.add((v1, v2))
+            print("V", v1, 1*v2)
+
+    state['Accumulator'] = byte(result)
+
+
 
 def STA(state, a) -> [(0x8D, imm2), (0x85, imm), (0x91, indy), (0x99, absy)]:
     state['Memory'][a] = state['Accumulator']
 def STX(state, a) -> [(0x86, imm)]: state['Memory'][a] = state['X']
 def TXA(state, a) -> [(0x8A, _)]: state['Accumulator'] = state['X']; Z_set(state, state['Accumulator']); N_set(state, state['Accumulator'])
+def TYA(state, a) -> [(0x98, _)]: state['Accumulator'] = state['Y']; Z_set(state, state['Accumulator']); N_set(state, state['Accumulator'])
+def TAX(state, a) -> [(0xAA, _)]: state['X'] = state['Accumulator']; Z_set(state, state['X']); N_set(state, state['X'])
 
-def TXS(state, a) -> [(0x9A, _)]: state['StackRegister'] = state['X']
+def TXS(state, a) -> [(0x9A, _)]: state['StackOffset'] = state['X']
 
 def BPL(state, a) -> [(0x10, imm)]:
     if state['StatusRegister']['Negative'] == 0:
+        state['ProgramCounter'] += np.int8(a)
+
+def BCC(state, a) -> [(0x90, imm)]:
+    if state['StatusRegister']['Carry'] == 0:
         state['ProgramCounter'] += np.int8(a)
 
 def BCS(state, a) -> [(0xB0, imm)]:
@@ -130,6 +192,13 @@ def JSR(state, a) -> [(0x20, imm2)]:
 
     state['ProgramCounter'] = a
 
+def NMI(state):
+    nmi_L = state['Memory'][0xFFFA]
+    nmi_H = state['Memory'][0xFFFB]
+    data = nmi_H*0x0100 + nmi_L
+
+    JSR(state, data)
+
 def RTS(state, a) -> [(0x60, _)]:
     state['StackOffset'] += 1
     pc_L = state['Memory'][state['StackZero'] + state['StackOffset']]
@@ -138,12 +207,22 @@ def RTS(state, a) -> [(0x60, _)]:
 
     state['ProgramCounter'] = (pc_H << 8) + pc_L
 
+def PHA(state, a) -> [(0x48, _)]:
+    state['Memory'][state['StackZero'] + state['StackOffset']] = state['Accumulator']
+    state['StackOffset'] -= 1
+
+def PLA(state, a) -> [(0x68, _)]:
+    state['StackOffset'] += 1
+    state['Accumulator'] = state['Memory'][state['StackZero'] + state['StackOffset']]
+
+    Z_set(state, state['Accumulator'])
+    N_set(state, state['Accumulator'])
 
 #-
 
 
 instructions = {}
-byte_counts = {_: 1, imm: 2, imm2: 3, abs_read: 3, abs_x_read: 3, indy: 2, absy: 3}
+byte_counts = {_: 1, imm: 2, imm2: 3, zpg: 2, abs_read: 3, abs_x_read: 3, indy: 2, absy: 3}
 for name, func in locals().copy().items():
     if settings := callable(func) and func.__annotations__.get('return'):
         for opcode, addressing in settings:
